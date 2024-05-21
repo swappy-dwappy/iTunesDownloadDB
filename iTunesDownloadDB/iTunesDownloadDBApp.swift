cf0f6e4c7e1550a5f0619d1865b837048162d481
swappy <rit2010016@gmail.com>
2024-05-21 06:19:29 +0530

    Initial local project setup committed

---
+++ iTunesDownloadDB/iTunesDownloadDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesDownloadDBApp.swift
+//  iTunesDownloadDB
+//
+//  Created by Sonkar, Swapnil on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iTunesDownloadDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
