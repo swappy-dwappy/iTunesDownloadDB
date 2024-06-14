8889bc6ded1e4f78925cbd44bcd0c83eff22e1d5
swappy <rit2010016@gmail.com>
2024-06-15 04:08:30 +0530

    PersistenceController class updated. Added func getEntity deleteEntity

--- iTunesDownloadDB/Database/PersistenceController.swift
+++ iTunesDownloadDB/Database/PersistenceController.swift
@@ -7,6 +7,9 @@
 
 import CoreData
 
+let persistenceController = PersistenceController.shared
+let mainContext = PersistenceController.shared.container.viewContext
+
 class PersistenceController: ObservableObject {
     static let shared = PersistenceController()
     
@@ -21,5 +24,77 @@ class PersistenceController: ObservableObject {
         
         self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         self.container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func saveContext() throws {
+        if mainContext.hasChanges {
+            try mainContext.save()
+        }
+    }
+    
+    lazy var privateContext: NSManagedObjectContext = {
+        return PersistenceController.shared.container.newBackgroundContext()
+    }()
+    
+}
+
+extension PersistenceController {
+    
+    func getEntity<E: NSManagedObject>() -> [E] {
+        let fetchRequest = E.fetchRequest()
+        do {
+            return try mainContext.fetch(fetchRequest) as? [E] ?? []
+        } catch {
+            print(error.localizedDescription)
+        }
+        return []
+    }
+    
+    func getSafeObject<E, S>(entity: E.Type) -> [S] where E: NSManagedObject, E: SafeObjectType, S == E.SafeType {
+        let entities = getEntity() as [E]
+        return entities.map {
+            $0.toSafeObject()
+        }
+    }
+    
+    func deleteAllEntities() {
+    }
+    
+    func deleteEntity(entityName: String) throws {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        deleteRequest.resultType = .resultTypeObjectIDs
+        
+        let batchDelete = try mainContext.execute(deleteRequest) as? NSBatchDeleteResult
+        
+        guard let deleteResult = batchDelete?.result as? [NSManagedObjectID] else { return }
+        
+        let deletedObjects: [AnyHashable: Any] = [NSDeletedObjectsKey: deleteResult]
+        
+        NSManagedObjectContext.mergeChanges(fromRemoteContextSave: deletedObjects, into: [mainContext])
+    }
+    
+    func deleteEntityInBackground(entityName: String) throws {
+        
+        try privateContext.performAndWait {
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            deleteRequest.resultType = .resultTypeObjectIDs
+            
+            let batchDelete = try privateContext.execute(deleteRequest) as? NSBatchDeleteResult
+
+            guard let deleteResult = batchDelete?.result as? [NSManagedObjectID] else { return }
+            
+            let deletedObjects: [AnyHashable: Any] = [NSDeletedObjectsKey: deleteResult]
+            
+            NSManagedObjectContext.mergeChanges(fromRemoteContextSave: deletedObjects, into: [mainContext])
+        }
+    }
+    
+    func deleteEntityInBackgroundAlternative(entityName: String) async throws {
+        
+        await persistenceController.container.performBackgroundTask { context in
+            
+        }
     }
 }
